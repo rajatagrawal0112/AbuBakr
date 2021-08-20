73f1886f9055ee77d83767882e8c19d8dec1f4c4
aashishquest <aashishporwal@questglt.org>
2021-08-21 00:15:26 +0530

    mail

--- helper/mailer.js
+++ helper/mailer.js
@@ -1,42 +1,42 @@
 const nodemailer = require("nodemailer");
-var smtpTransport2 = require('nodemailer-smtp-transport');
+// var smtpTransport2 = require('nodemailer-smtp-transport');
 async function mail(to_mail, mail_subject, mail_content) {
 
     const mailOptions = {
         to: to_mail,
-        from: 'info@ebtico.com',
+        from: 'ebticoglt@gmail.com',
         subject: mail_subject,
         html: mail_content
     };
 
-    // const smtpTransport = nodemailer.createTransport({
-    //     // host: 'smtp.zoho.in',
-    //     // port: 465,
-    //     // secure: true,
-    //     // auth: {
-    //     //     user: 'marketing@theartw.com',
-    //     //     pass: 'fDiG4jxUIGP7'
-    //     // }
-    //     service: 'gmail',
-    //     host: 'smtp.gmail.com',
-    //     auth: {
-    //         user: 'info@ebtico.com',
-    //         pass: 'bitebitco'
-    //     }
-    // });
+    const smtpTransport = nodemailer.createTransport({
+        // host: 'smtp.zoho.in',
+        // port: 465,
+        // secure: true,
+        // auth: {
+        //     user: 'marketing@theartw.com',
+        //     pass: 'fDiG4jxUIGP7'
+        // }
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        auth: {
+             user: 'ebticoglt@gmail.com',
+                pass: 'bitebitco'
+        }
+    });
 
     
-    const smtpTransport = nodemailer.createTransport(smtpTransport2({
-        port: 465,
-        // host: 'mail.ebtico.com',
-        auth: {
-          user: 'info@ebtico.com',
-          pass: 'bitebitco'
-        },
-        tls: {
-          rejectUnauthorized: false
-        }
-      }));
+    // const smtpTransport = nodemailer.createTransport(smtpTransport2({
+    //     port: 465,
+    //     // host: 'mail.ebtico.com',
+    //     auth: {
+    //       user: 'ebticoglt@gmail.com',
+    //       pass: 'bitebitco'
+    //     },
+    //     tls: {
+    //       rejectUnauthorized: false
+    //     }
+    //   }));
 
     return new Promise((resolve, reject) => {
         smtpTransport.sendMail(mailOptions, function (err, result) {
