677110b12bf3e6642c5086c64f1f6d5c9b01e7a1
aashishquest <aashishporwal@questglt.org>
2021-08-20 23:46:51 +0530

    mail

--- helper/mailer.js
+++ helper/mailer.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-
+var smtpTransport2 = require('nodemailer-smtp-transport');
 async function mail(to_mail, mail_subject, mail_content) {
 
     const mailOptions = {
@@ -9,21 +9,34 @@ async function mail(to_mail, mail_subject, mail_content) {
         html: mail_content
     };
 
-    const smtpTransport = nodemailer.createTransport({
-        // host: 'smtp.zoho.in',
-        // port: 465,
-        // secure: true,
-        // auth: {
-        //     user: 'marketing@theartw.com',
-        //     pass: 'fDiG4jxUIGP7'
-        // }
-        service: 'gmail',
-        host: 'smtp.gmail.com',
+    // const smtpTransport = nodemailer.createTransport({
+    //     // host: 'smtp.zoho.in',
+    //     // port: 465,
+    //     // secure: true,
+    //     // auth: {
+    //     //     user: 'marketing@theartw.com',
+    //     //     pass: 'fDiG4jxUIGP7'
+    //     // }
+    //     service: 'gmail',
+    //     host: 'smtp.gmail.com',
+    //     auth: {
+    //         user: 'info@ebtico.com',
+    //         pass: 'bitebitco'
+    //     }
+    // });
+
+    
+    const smtpTransport = nodemailer.createTransport(smtpTransport2({
+        port: 465,
+        // host: 'mail.ebtico.com',
         auth: {
-            user: 'info@ebtico.com',
-            pass: 'bitebitco'
+          user: 'info@ebtico.com',
+          pass: 'bitebitco'
+        },
+        tls: {
+          rejectUnauthorized: false
         }
-    });
+      }));
 
     return new Promise((resolve, reject) => {
         smtpTransport.sendMail(mailOptions, function (err, result) {
