8740cec4c252357b3a4a5a804b4d1c33575f4490
aashishquest <aashishporwal@questglt.org>
2021-08-21 00:40:33 +0530

    mail

--- helper/mailer.js
+++ helper/mailer.js
@@ -4,7 +4,7 @@ async function mail(to_mail, mail_subject, mail_content) {
 
     const mailOptions = {
         to: to_mail,
-        from: 'ebticoglt@gmail.com',
+        from: 'shanakhan@questglt.org',
         subject: mail_subject,
         html: mail_content
     };
@@ -20,8 +20,10 @@ async function mail(to_mail, mail_subject, mail_content) {
         service: 'gmail',
         host: 'smtp.gmail.com',
         auth: {
-             user: 'ebticoglt@gmail.com',
-                pass: 'bitebitco'
+            //  user: 'ebticoglt@gmail.com',
+            //     pass: 'bitebitco'
+            user: 'shanakhan@questglt.org',
+            pass: 'MomDad@789'
         }
     });
 
